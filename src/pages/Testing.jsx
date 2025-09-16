b3a635d13ea20a996a0523f52decd8bdffe337ea
Ansari <ansarikatugusman@gmail.com>
2025-09-16 23:31:27 +0800

    Tested character imiage for dialogue component

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,17 +1,21 @@
 import './Testing.css'
 import '../components/buttons/SketchedButton.css'
+import Hiraya from '../assets/images/hiraya.png'
 
 const Testing = () => {
     return (
-        <button
-                className={`sketch_button`}
-                style={{width: '200px'}}
-
-            >
-                <span className='sketch_button_span' id='1456' onClick={(e) => {
-                    console.log(e.target.id)
-                }}>HELLO WORLD</span>
-            </button>
+        <div className='dialogue-container modal'>
+            <div className='dialouge_character-wrapper ohph'>
+                <img className='dialouge_character' src={Hiraya} />
+            </div>
+            <div className='dialouge_text-wrapper' style={{ display: 'none' }}>
+                <div className='dialouge_text'>
+                    <p>
+                        Thank you for visiting Hiraya Hangman. We're currently in pilot testing, and we're excited to have you try things out. More feautures will be implemented in the following month. Your feedback helps us make this website better, so feel free to look around and let us know what you think!
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
