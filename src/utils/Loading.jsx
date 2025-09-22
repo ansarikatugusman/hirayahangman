f3d8f3cbf8cc4cc4d03c4eb1754f57700132b805
Ansari <ansarikatugusman@gmail.com>
2025-09-22 21:08:43 +0800

    Add loading spinner component and fetch request calbback

---
+++ src/utils/Loading.jsx
@@ -0,0 +1,11 @@
+import './Loading.css';
+
+const Loading = () => {
+    return (
+        <div className='loading_spinner_overlay'>
+            <div className="loading_spinner"></div>
+        </div>
+    )
+}
+
+export default Loading;
