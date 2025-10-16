220d44c4eeba447b59bf147555fe919a0be307af
Ansari <ansarikatugusman@gmail.com>
2025-10-16 13:00:03 +0800

    Relocated vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
         <BrowserRouter>
-            <Analytics />
             <App />
         </BrowserRouter>
    </GoogleOAuthProvider>
