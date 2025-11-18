5d73d8d4c3c29426d4ffb20f954127c6d59e9fbd
Ansari <ansarikatugusman@gmail.com>
2025-11-18 23:29:23 +0800

    Updated credits

--- src/menus/Credits.jsx
+++ src/menus/Credits.jsx
@@ -10,7 +10,7 @@ const Credits = ({ closeCreditsMenu }) => {
                     CREDITS
                 </div>
                 <div className='credits-wrapper ohpw center'>
-                    <div className='credit-wrapper center'>
+                    <div className='credit_single-wrapper center'>
                         <div className='credit_name center'>
                             JHONLIRRY P. MERCURIO
                         </div>
@@ -19,7 +19,7 @@ const Credits = ({ closeCreditsMenu }) => {
                         </div>
                     </div>
 
-                    <div className='credit-wrapper center'>
+                    <div className='credit_single-wrapper center'>
                         <div className='credit_name center'>
                             SHAN MICKLE RECAÑA
                         </div>
@@ -28,7 +28,7 @@ const Credits = ({ closeCreditsMenu }) => {
                         </div>
                     </div>
 
-                    <div className='credit-wrapper center'>
+                    <div className='credit_single-wrapper center'>
                         <div className='credit_name center'>
                             JESUN RODRIGO
                         </div>
@@ -37,12 +37,306 @@ const Credits = ({ closeCreditsMenu }) => {
                         </div>
                     </div>
 
-                    <div className='credit-wrapper center'>
+                    <div className='credit_single-wrapper center'>
                         <div className='credit_name center'>
                             ANSARI K. USMAN
                         </div>
                         <div className='credit center'>
                             Game Design & Programming
+                        </div>
+                    </div>
+
+                    <div className='credit_single-wrapper center'>
+                        <div className='credit_name center'>
+                            ALJAY P. MERCURIO (KAPEBEOVORUS)
+                        </div>
+                        <div className='credit center'>
+                            Art / Character Design
+                        </div>
+                    </div>
+                    
+                    <div className='credit_single-wrapper center'>
+                        <div className='credit_name center'>
+                            JOEL S. JIMENEZ
+                        </div>
+                        <div className='credit center'>
+                            Project Advising / Consulting
+                        </div>
+                    </div>
+
+                    <div className='credit_multiple-wrapper center'>    
+                        <div className='credit center'>
+                            Special Thanks
+                        </div>
+                        <div className='credit_name center'>
+                            IRIS MECHELLE L. GARGAR 
+                        </div>
+                        <div className='credit_name center'>
+                            MICHAEL ARIEL CABUGAO
+                        </div>
+                        <div className='credit_name center'>
+                            FREDERICK RUNAS
+                        </div>
+                        <div className='credit_name center'>
+                            ALLONDRA PAULINE FARRO
+                        </div>
+                    </div>
+
+                    <div className='credit_multiple-wrapper center'>    
+                        <div className='credit_name center'>
+                            RYAN LORENSEN
+                        </div>
+                        <div className='credit_name center'>
+                            HZSMITH
+                        </div>
+                        <div className='credit_name center'>
+                            THEOALLEN
+                        </div>
+                        <div className='credit_name center'>
+                            RAWIZ
+                        </div>
+                        <div className='credit_name center'>
+                            ZAKIRO
+                        </div>
+                        <div className='credit_name center'>
+                            LAI LIN_FORREST
+                        </div>
+                        <div className='credit_name center'>
+                            RENEE RUBEN
+                        </div>
+                        <div className='credit_name center'>
+                            CRISTEN CONGER
+                        </div>
+                        <div className='credit_name center'>
+                            EMILY OSTERLOFF
+                        </div>
+                        <div className='credit_name center'>
+                            STEVE BLANCHARD
+                        </div>
+                        <div className='credit_name center'>
+                            MICHAEL BROWN
+                        </div>
+                        <div className='credit_name center'>
+                            ERIC ROBERTS
+                        </div>
+                        <div className='credit_name center'>
+                            BRO VECTOR
+                        </div>
+                        <div className='credit_name center'>
+                            ILIANA CABRERA
+                        </div>
+                        <div className='credit_name center'>
+                            ANXIOUS223
+                        </div>
+                        <div className='credit_name center'>
+                            BAMBI L. DINGMAN
+                        </div>
+                        <div className='credit_name center'>
+                            SGLIPUT
+                        </div>
+                        <div className='credit_name center'>
+                            OLLIE TOMS
+                        </div>
+                        <div className='credit_name center'>
+                            ANANT90
+                        </div>
+                        <div className='credit_name center'>
+                            DOTDASH
+                        </div>
+                        <div className='credit_name center'>
+                            BEN SOCKOL
+                        </div>
+                        <div className='credit_name center'>
+                            JASON HOLLAND
+                        </div>
+                        <div className='credit_name center'>
+                            ERIC ROCK
+                        </div>
+                        <div className='credit_name center'>
+                            AJOKE
+                        </div>
+                        <div className='credit_name center'>
+                            MIKE SZYDLOWSKI
+                        </div>
+                        <div className='credit_name center'>
+                            LOWENA HULL
+                        </div>
+                        <div className='credit_name center'>
+                            BRAD IMHOFF
+                        </div>
+                        <div className='credit_name center'>
+                            JOY LANZENDORFER
+                        </div>
+                        <div className='credit_name center'>
+                            MONICA CULL
+                        </div>
+                        <div className='credit_name center'>
+                            CATHERINE STODDARD
+                        </div>
+                        <div className='credit_name center'>
+                            SARUTOBII2
+                        </div>
+                        <div className='credit_name center'>
+                            MICHAEL HESSION
+                        </div>
+                        <div className='credit_name center'>
+                            CEMILE BINGOL
+                        </div>
+                        <div className='credit_name center'>
+                            REDBUBBLE
+                        </div>
+                        <div className='credit_name center'>
+                            ROMMEL RAMOS
+                        </div>
+                        <div className='credit_name center'>
+                            SHEELA PHILOMENA
+                        </div>
+                        <div className='credit_name center'>
+                            AMANDA MCDONALD
+                        </div>
+                        <div className='credit_name center'>
+                            ASHLEY POMEROY
+                        </div>
+                        <div className='credit_name center'>
+                            ELIZABETH GAMILLO
+                        </div>
+                        <div className='credit_name center'>
+                            CARL FREDRICKSEN
+                        </div>
+                        <div className='credit_name center'>
+                            CRIM.SONSIN
+                        </div>
+                        <div className='credit_name center'>
+                            ERIN HUFFSTETLER
+                        </div>
+                        <div className='credit_name center'>
+                            MATT MOLLOY
+                        </div>
+                        <div className='credit_name center'>
+                            JULIO VISKOVICH
+                        </div>
+                        <div className='credit_name center'>
+                            VOTEDEADSEA
+                        </div>
+                        <div className='credit_name center'>
+                            RACHAEL MOELLER GORMAN
+                        </div>
+                        <div className='credit_name center'>
+                            NICOLE CORD-CRUZ
+                        </div>
+                        <div className='credit_name center'>
+                            KHAOSAMING
+                        </div>
+                        <div className='credit_name center'>
+                            AUGUSTA FALLETTA
+                        </div>
+                        <div className='credit_name center'>
+                            CARLYLE EDMUNSON
+                        </div>
+                        <div className='credit_name center'>
+                            ULET IFANSASTI
+                        </div>
+                        <div className='credit_name center'>
+                            IGOR TSUKANOV
+                        </div>
+                        <div className='credit_name center'>
+                            SHINU281
+                        </div>
+                        <div className='credit_name center'>
+                            CRYSTAL GORNTO
+                        </div>
+                        <div className='credit_name center'>
+                            RIKUDOU44
+                        </div>
+                        <div className='credit_name center'>
+                            ALBERTO PATRICIO SEGURA
+                        </div>
+                        <div className='credit_name center'>
+                            CHRIS HINTON
+                        </div>
+                        <div className='credit_name center'>
+                            OMAR GALLAGA
+                        </div>
+                        <div className='credit_name center'>
+                            BILLY HUYNH
+                        </div>
+                        <div className='credit_name center'>
+                            BETTY LIN-FISHER
+                        </div>
+                        <div className='credit_name center'>
+                            NEKKI
+                        </div>
+                        <div className='credit_name center'>
+                            ADITHI RAMAKRISHNAN
+                        </div>
+                        <div className='credit_name center'>
+                            PATRICK CHANDTER
+                        </div>
+                        <div className='credit_name center'>
+                            SUJIT KUMAR
+                        </div>
+                        <div className='credit_name center'>
+                            ASHLEY STRICKLAND
+                        </div>
+                        <div className='credit_name center'>
+                            DAGA MARINSD
+                        </div>
+                        <div className='credit_name center'>
+                            ARCONSOLI
+                        </div>
+                        <div className='credit_name center'>
+                            DAVID ECKOFF
+                        </div>
+                        <div className='credit_name center'>
+                            MOBUCKEYE
+                        </div>
+                        <div className='credit_name center'>
+                            REX RUTKOSKI
+                        </div>
+                        <div className='credit_name center'>
+                            TUOLEMNE MEADOWS
+                        </div>
+                        <div className='credit_name center'>
+                            NOA LEACH
+                        </div>
+                        <div className='credit_name center'>
+                            MARBELLA PEREZ
+                        </div>
+                        <div className='credit_name center'>
+                            AKSHAY HARI
+                        </div>
+                        <div className='credit_name center'>
+                            DAN BORMANN
+                        </div>
+                        <div className='credit_name center'>
+                            PABLO A. TARIMAN
+                        </div>
+                        <div className='credit_name center'>
+                            KRISTINA MARI BALTAZAR
+                        </div>
+                        <div className='credit_name center'>
+                            CIELO ANNE CALZADO-MENESES
+                        </div>
+                        <div className='credit_name center'>
+                            CHERRY CAMACHO
+                        </div>
+                        <div className='credit_name center'>
+                            JAMES MACDONALD
+                        </div>
+                        <div className='credit_name center'>
+                            DENICE CALUB
+                        </div>
+                        <div className='credit_name center'>
+                            ELIZABETH MCDONALD
+                        </div>
+                        <div className='credit_name center'>
+                            TED LITCHFIELD
+                        </div>
+                        <div className='credit_name center'>
+                            CRISPIN GARICA
+                        </div>
+                        <div className='credit_name center'>
+                            BRYAN MCWHORTER
                         </div>
                     </div>
                 </div>
